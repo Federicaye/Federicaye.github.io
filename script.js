c25b0a855741e1862ef59c17ec096f875629c5d9
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-09-16 12:00:44 +0200

    fetch

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 let ciaoButton=document.getElementById('ciaoButton');
 ciaoButton.addEventListener('click', () => {
 let p = document.createElement('p');
-p.innerText='ciao';
+p.innerText='ciaou';
 document.body.appendChild(p);
+console.log('ciao');
 })
+
+async function populate() {
+    const requestURL ="projects.json";
+      const request = new Request(requestURL);
+      const response = await fetch(request);
+      const projects = await response.json();
+      console.log('ciao');
+    }
+
+window.addEventListener('DOMContentLoaded', () => {
+    populate();
+    console.log('ciao');
+}); 
